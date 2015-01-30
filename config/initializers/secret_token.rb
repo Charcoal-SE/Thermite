b25463aed6c234ec0f9d1a5852622e338b43ba34
Undo1 <parker@erwaysoftware.com>
2015-01-29 17:05:41 -0700

    Rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thermite::Application.config.secret_token = '63f5a9560f638dd0ac138e595f71ed75202d4e7fe4c7c4dcc549a6ee700c90ce267e6b2e0b6c3459f95203abf95757ad22f0a90fd3e13502330145313ec4d80b'
