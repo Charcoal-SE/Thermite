02d0590d6bbaa6eeafb118cbd76ab065480680a8
Undo1 <parker@erwaysoftware.com>
2015-01-29 17:21:01 -0700

    awesome stuff

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.21'
 
 gem 'sqlite3'
 
+gem "twitter-bootstrap-rails"
+gem "awesome_print"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Thermite::Application.routes.draw do
+  resources :comments
+
+
+  resources :sites
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
