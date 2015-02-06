e6ff6be286bc79a4608c45af38eefa486b95b326
Undo1 <parker@erwaysoftware.com>
2015-02-05 18:55:24 -0700

    maybe?

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,5 +1,5 @@
 class HistoryController < ApplicationController
   def index
-    @recent_flags = FlagHandlings.order(created_at: :desc).first(100)
+    @recent_flags = FlagHandlings.order(created_at: :asc).first(100)
   end
 end
